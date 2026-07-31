e2c01bdb55db0358f91eb3658743d7746a7f0cb9
Aryanhapani <hapaniaryan51@gmail.com>
2026-07-31 09:44:42 +0530

    H

--- arrays/find_element_duplicate.java
+++ arrays/find_element_duplicate.java
@@ -1,16 +1,23 @@
 public class find_element_duplicate {
     public static void main(String[] args) {
         int arr[]={1,3,4,5,3,4};
-
+        int x=3;
+        int count=0;
         for(int i=0;i<arr.length;i++){
 
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                    System.out.println(arr[i]);
+            
+                if(arr[i]==x){
+                    count++;
                     
                 }
             }
+            if(count >= 2){
+                System.out.println("duplicate");
+            }else{
+                System.out.println("not");
+            }
+
         }
-        System.out.println("not find dublicate");
+        
     }
-}
+
--- arrays/find_unique_element.java
+++ arrays/find_unique_element.java
@@ -1,18 +1,21 @@
 public class find_unique_element {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,3};
+        int x=4;
+        int count=0;
 
         for(int i=0;i<arr.length;i++){
-            int count=0;
 
-            for(int j=0;j<arr.length;j++){
-                if(arr[i]==arr[j]){
+           
+                if(arr[i]==x){
                     count++;
                 }
+            
             }
             if (count < 2) {
-                System.out.println(arr[i]);
+                System.out.println("unique");
+            }else{
+                System.out.println("not");
             }
-        }
     }
 }
